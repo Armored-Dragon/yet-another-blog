f4bf5c37d906d65d0cf9e7df5535307aac088028
Armored Dragon <publicmail@armoreddragon.com>
2024-07-08 13:17:53 -0500

    Added eslint. Removed old linter.
    Signed-off-by: Armored Dragon <publicmail@armoreddragon.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,4 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } }, { languageOptions: { globals: globals.browser } }, { rules: { "no-unused-vars": "error", "no-undef": "error", indent: ["error", "tab", { tabWidth: 4 }] } }, pluginJs.configs.recommended];
